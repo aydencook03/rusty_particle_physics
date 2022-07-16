78aac065aa2188bb7b987a8b3531f80fd6a51c58
Ayden Cook <cookayden2003@gmail.com>
2022-07-16 14:32:40 -0400

    added lifetime parameters to Force

--- engine_2d/src/dynamics/force.rs
+++ engine_2d/src/dynamics/force.rs
@@ -1,16 +1,15 @@
-//use crate::vec2::Vec2;
-//use crate::matter::particle::Particle;
-//use super::constraint::Constraint;
+use crate::vec2::Vec2;
+use crate::matter::particle::Particle;
+use super::constraint::Constraint;
 
 /// Force on a Particle or between interacting Particles
-pub enum Force {
-    /*
+pub enum Force<'a> {
     /// The fundamental type of 2d force
     ///
     /// The other Force types usually return a configured GeneralForce when being handled
     GeneralForce {
-        particle1: Option<&mut Particle>,
-        particle2: &mut Particle,
+        particle1: Option<&'a mut Particle>,
+        particle2: &'a Particle,
         force: Vec2,
     },
 
@@ -21,31 +20,30 @@ pub enum Force {
     /// - dampened spring between Particles { LinkFixedDistance, stiffness, 1, 0.5 }
     /// - stiff pendulum { FixedDistance, 99, 1, 99 }
     ConstraintForce {
-        constraint_type: Constraint,
+        constraint_type: Constraint<'a>,
         k: f64,
         n: f64,
         b: f64,
     },
 
     /// A simple downwards pull of gravity (F = -mg)
-    WorldGravity { particle: &mut Particle, g: f64 },
+    WorldGravity { particle: &'a mut Particle, g: f64 },
 
     /// Newtonian gravitational attraction between two Particles
     Gravity {
-        particle1: &mut Particle,
-        particle2: &mut Particle,
+        particle1: &'a mut Particle,
+        particle2: &'a mut Particle,
         G: f64,
     },
 
     /// Drag force. [Wikipedia](https://en.wikipedia.org/wiki/Drag_equation)
     Drag {
-        particle: &mut Particle,
+        particle: &'a mut Particle,
         strength: f64,
     },
-    */
 }
 
-impl Force {
+impl<'a> Force<'a> {
     /// Send the Force to the Particle(s)
     pub fn send_force(self: &Self) {
         todo!();
