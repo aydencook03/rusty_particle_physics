14d1c532d8101a4773ce6be215658fb57a55fd4d
Ayden Cook <cookayden2003@gmail.com>
2022-07-19 17:48:32 -0400

    rgb to u32 conversion helper function

--- minimal_renderer/src/lib.rs
+++ minimal_renderer/src/lib.rs
@@ -1,7 +1,7 @@
 use rusty_particle_physics_2d::sim::Sim;
 
 use winit::{
-    dpi::LogicalSize,
+    dpi::PhysicalSize,
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
@@ -12,9 +12,10 @@ use softbuffer::GraphicsContext;
 pub struct MinimalRenderer {
     pub width: u32,
     pub height: u32,
+    pub bg_color: (u32, u32, u32),
     event_loop: EventLoop<()>,
-    pub context: GraphicsContext<Window>,
-    pub buffer: Vec<u32>,
+    context: GraphicsContext<Window>,
+    buffer: Vec<u32>,
 }
 
 impl MinimalRenderer {
@@ -25,26 +26,43 @@ impl MinimalRenderer {
         let event_loop = EventLoop::new();
 
         let window = {
-            let size = LogicalSize::new(width, height);
+            let size = PhysicalSize::new(width, height);
             WindowBuilder::new()
                 .with_inner_size(size)
+                .with_max_inner_size(size)
                 .with_title("Simulation")
                 .build(&event_loop)
                 .unwrap()
         };
 
-        let buffer_len = width * height;
+        let buffer_len: usize = (width * height).try_into().unwrap();
+
+        let bg_color: (u32, u32, u32) = (70, 70, 70);
+
+        let start_color = MinimalRenderer::rgb_to_u32(bg_color);
 
         MinimalRenderer {
             width,
             height,
+            bg_color,
             event_loop,
             context: unsafe { GraphicsContext::new(window) }.unwrap(),
-            buffer: Vec::with_capacity(buffer_len.try_into().unwrap()),
+            buffer: vec![start_color; buffer_len],
         }
     }
 
-    pub fn run(self: Self, _sim: &mut Sim) {
+    /// Converts from an rgb color to the format that softbuffer uses.
+    /// Pixel format (u32): 00000000RRRRRRRRGGGGGGGGBBBBBBBB
+    pub fn rgb_to_u32(rgb: (u32, u32, u32)) -> u32 {
+        let (r, g, b) = rgb;
+        let r = r << 16;
+        let g = g << 8;
+        let b = b << 0;
+
+        r | g | b
+    }
+
+    pub fn run(mut self: Self, _sim: &mut Sim) {
         let event_loop = self.event_loop;
         event_loop.run(move |event, _, control_flow| {
             // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
@@ -61,6 +79,11 @@ impl MinimalRenderer {
                 }
                 Event::MainEventsCleared => {
                     // update & render
+                    self.context.set_buffer(
+                        &self.buffer,
+                        self.width.try_into().unwrap(),
+                        self.height.try_into().unwrap(),
+                    );
                 }
                 _ => (),
             }
